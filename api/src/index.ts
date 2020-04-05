240839e47244e30fae0ad28e7da752e704cf5eb8
Nguyen <knguyen30111@gmail.com>
2020-04-05 14:46:32 +0900

    add abstract handle create user

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,29 +1,26 @@
-import express from 'express'
+
 import session from 'express-session'
 import connectRedis from 'connect-redis'
 import Redis from 'ioredis'
 import mongoose from 'mongoose'
 import { 
   REDIS_OPTIONS, 
-  SESSION_OPTIONS, 
   APP_PORT, 
   MONGO_URI, 
   MONGO_OPTIONS 
 } from './config'
+import { createApp } from './app'
 
 ;(async () => {
   await mongoose.connect(MONGO_URI, MONGO_OPTIONS)
+
   const RedisStore = connectRedis(session)
+
   const client = new Redis(REDIS_OPTIONS)
+  
+  const store = new RedisStore({ client })
 
-  const app = express()
-
-  app.use(session({
-    ...SESSION_OPTIONS,
-    store: new RedisStore({ client })
-  }))
-
-  app.get('/', (req, res) => res.json({ message: 'OK' }))
+  const app = createApp(store)
 
   app.listen(APP_PORT, () => console.log(`http://localhost:${APP_PORT}`))
 })()
