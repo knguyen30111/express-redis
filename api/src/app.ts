0421c0dcf9d2d816499f84d4de7135d549ce619d
Nguyen <knguyen30111@gmail.com>
2020-04-05 14:51:32 +0900

    handle createUser

---
+++ api/src/app.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+import session, { Store } from 'express-session'
+import { SESSION_OPTIONS } from './config'
+import { register } from './routes'
+
+export const createApp = (store: Store) => {
+  const app = express()
+
+  app.use(express.json())
+
+  app.use(session({
+    ...SESSION_OPTIONS,
+    store,
+  }))
+
+  app.use(register)
+
+  return app
+} 
+
+
